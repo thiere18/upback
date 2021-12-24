b8b567c0ab1c3717eec7c3ec1673f9533023e23f
thierno <thiernoibrahimalo@esp.sn>
2021-12-24 17:49:41 +0000

    working well

--- backend/app/alembic/versions/91979b40eb38_create_users_table.py
+++ backend/app/alembic/versions/91979b40eb38_create_users_table.py
@@ -27,6 +27,12 @@ def upgrade():
         sa.Column("address", sa.String(100)),
         sa.Column("hashed_password", sa.String(100), nullable=False),
         sa.Column("is_active", sa.Boolean, nullable=False),
+        sa.Column("restricted_areas", sa.String(100)),
+        sa.Column("permitted_areas",sa.String(100)),
+        sa.Column("restricted_sources",sa.String(100)),
+        sa.Column("permitted_sources",sa.String(100)),
+        sa.Column("restricted_tags", sa.String(100)),
+        sa.Column("permitted_tags", sa.String(100)),
         # sa.Column("is_superuser", sa.Boolean, nullable=False),
         sa.Column("role", sa.String(100),nullable=False,server_default=sa.text('user'),),
     )
